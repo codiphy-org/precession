c5f85b6c3f8e202a50da75310d5487c599a2f786
Ashwin Kamath <ashwinkn@hotmail.com>
2022-03-19 14:36:11 -0500

    Updated test snapshots

--- src/precession/tests/test_planet.py
+++ src/precession/tests/test_planet.py
@@ -3,22 +3,24 @@ from precession.config import Config
 import yaml
 
 
-def test_planet_load_dict():
+def test_planet_load_dict(snapshot):
     planet = Planet.load(Config(), {
         "M": 30,
     })
 
-    assert planet.M == 30
+    snapshot.assert_match(yaml.safe_dump(
+        planet.get_dict()), 'planet_object.yml')
 
 
-def test_planet_serialize(tmp_path):
+def test_planet_serialize(tmp_path, snapshot):
     planet = Planet.load(Config(), {
         "M": 30,
     })
     planet.save(tmp_path/"planet.yml")
     planet1 = Planet.load(Config(), tmp_path/"planet.yml")
 
-    assert planet1.M == 30
+    snapshot.assert_match(yaml.safe_dump(
+        planet1.get_dict()), 'planet_object.yml')
 
 
 def test_planet_fixups(snapshot):
