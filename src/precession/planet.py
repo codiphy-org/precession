e0cce10a67900c823b03c892e07ce315fa7a0452
Ashwin Kamath <ashwinkn@hotmail.com>
2022-03-19 13:20:35 -0500

    Added more planet code

--- src/precession/planet.py
+++ src/precession/planet.py
@@ -1,12 +1,47 @@
 import yaml
 import pathlib
 import json
+import math
 
 
 class Planet(object):
     def __init__(self):
-        self.mass = 0
+        self.GMS = 1.
+        # mass
+        self.M = 0.
         self.name = "unknown"
+        # period
+        self.T = 1.
+        # eccentricity
+        self.e = 0.
+        # semi major axis
+        self.a = 1.
+
+        # Need to figure out how to do this better
+        self.decoupledMercury = False
+        self.grMercury = False
+
+    def fixup(self):
+        self.RMin = self.a * (1 - self.e)
+        self.RMax = self.a * (1 + self.e)
+        self.vMax = math.sqrt(
+            (((1 + self.e) * (1 + self.M)) / self.RMin) * self.GMS)
+        self.L = self.a * (1 - self.e) * self.vMax
+        if (self.decoupledMercury):
+            self.GMM = 0.
+        else:
+            self.GMM = self.GMS * self.M
+        if (self.grMercury):
+            # Constant for General Relativistic correction on Mercury due to Sun
+            # This is the value that goes into the force equation term as:
+            #  +3*GMS*alpha/r^4
+            # And update to potential term in Lagrangian will be something like:
+            #  -GMS*alpha/r^3
+            # Where 3*alpha value taken from Giordano & Nakanishi is: 1.1e-8
+            # self.GMR = self.GMS * (1.1e-8)
+            self.GMR = self.GMS / (6.25e+4)**2
+        else:
+            self.GMR = 0.
 
     @staticmethod
     def load(data):
@@ -20,8 +55,12 @@ class Planet(object):
         planet = Planet()
         for k in data:
             setattr(planet, k, data[k])
+        planet.fixup()
         return planet
 
     def save(self, filename):
         with open(filename, 'w') as file:
             yaml.dump(self.__dict__, file)
+
+    def __str__(self) -> str:
+        return f"planet {self.name} => {', '.join(yaml.safe_dump(self.__dict__).splitlines())}"
--- src/precession/tests/test_planet.py
+++ src/precession/tests/test_planet.py
@@ -1,23 +1,36 @@
 from precession.planet import Planet
+import yaml
 
 
 def test_planet_load_dict():
     planet = Planet.load({
-        "mass": 30,
-        "radius": 50
+        "M": 30,
+        "R": 50
     })
 
-    assert planet.mass == 30
-    assert planet.radius == 50
+    assert planet.M == 30
+    assert planet.R == 50
 
 
 def test_planet_serialize(tmp_path):
     planet = Planet.load({
-        "mass": 30,
-        "radius": 50
+        "M": 30,
+        "R": 50,
     })
     planet.save(tmp_path/"planet.yml")
     planet1 = Planet.load(tmp_path/"planet.yml")
 
-    assert planet1.mass == 30
-    assert planet1.radius == 50
+    assert planet1.M == 30
+    assert planet1.R == 50
+
+
+def test_planet_fixups(snapshot):
+    planet = Planet.load({
+        "M": 30.,
+        "R": 50.,
+        "e": 0.1,
+        "a": 0.2,
+        "T": 0.5
+    })
+    snapshot.assert_match(f"{planet}", 'planet_string.yml')
+    snapshot.assert_match(yaml.safe_dump(planet.__dict__), 'planet_object.yml')
